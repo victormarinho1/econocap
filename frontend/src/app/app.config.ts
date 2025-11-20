e917c8975708ca87cddcece03887470f961d2b92
victormarinho1.com <victorhugoferreira452@gmail.com>
2025-11-20 14:11:46 -0300

    feat: add login page

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient} from '@angular/common/http';
+import { provideHttpClient, withFetch} from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -11,6 +12,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
      providePrimeNG({
             theme: {
                 preset: Aura
---
+++ frontend/src/app/auth/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';;
+import { PasswordModule } from 'primeng/password';
+
+//@ts-ignore
+import JustValidate from 'just-validate';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule, ButtonModule, CheckboxModule, InputTextModule,ReactiveFormsModule,PasswordModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login  {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+  checked1 = signal<boolean>(true);
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulário enviado:', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched(); // Mostra erros
+    }
+  }
+}
