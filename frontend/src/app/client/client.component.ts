d1dbe5ff255da820b51e6c7552081e83aabe8f0a
victormarinho1.com <victorhugoferreira452@gmail.com>
2025-11-22 14:35:45 -0300

    add footer to main page

--- frontend/src/app/client/client.component.ts
+++ frontend/src/app/client/client.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../shared/components/header/header";
 import { AdminRoutingModule } from "../admin/admin-routing-module";
+import { FooterComponent } from "../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-client',
-  imports: [Header, AdminRoutingModule],
+  imports: [Header, AdminRoutingModule, FooterComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css',
 })
---
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-footer',
+  imports: [InputText],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+
+}
